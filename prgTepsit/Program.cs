d99ccbde77ef8f6965e5b56abb36100e24c7c2b6
Nico <nicoconvito04@gmail.com>
2020-11-03 12:24:25 +0100

    Convertitore di un numero

--- prgTepsit/Program.cs
+++ prgTepsit/Program.cs
@@ -17,40 +17,11 @@ namespace prgTepsit
             divisore = Convert.ToInt32(Console.ReadLine());
             while (numero != 0)
             {
+
                 resto = numero % divisore;
-                numero = numero / 16;
+                numero = numero / 2;
                 numeroConvertito = Convert.ToString(resto) + numeroConvertito;
-                if (resto > 9)
-                {
-                    if (resto == 10)
-                    {
-                        numeroConvertito = "A" + numeroConvertito;
-                    }
-                    else if (resto == 11)
-                    {
-                        numeroConvertito = "B" + numeroConvertito;
-                    }
-                    else if (resto == 12)
-                    {
-                        numeroConvertito = "C" + numeroConvertito;
-                    }
-                    else if (resto == 13)
-                    {
-                        numeroConvertito = "D" + numeroConvertito;
-                    }
-                    else if (resto == 14)
-                    {
-                        numeroConvertito = "E" + numeroConvertito;
-                    }
-                    else if (resto == 15)
-                    {
-                        numeroConvertito = "F" + numeroConvertito;
-                    }
-                    else
-                    {
-                        numeroConvertito = Convert.ToString(resto) + numeroConvertito;
-                    }
-                }
+               
             }
             Console.WriteLine($"Il numero convertito è {numeroConvertito}");
             Console.ReadLine();
