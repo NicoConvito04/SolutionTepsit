c38376056ce73e8e0e500d0dcb7088604086c1a6
Nico <nicoconvito04@gmail.com>
2020-11-03 12:19:58 +0100

    Convertitore di un numero decimale

--- prgTepsit/Program.cs
+++ prgTepsit/Program.cs
@@ -6,7 +6,54 @@ namespace prgTepsit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero;
+            int resto;
+            string numeroConvertito = "";
+            int divisore;
+
+            Console.WriteLine("Inserisci un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci divisore");
+            divisore = Convert.ToInt32(Console.ReadLine());
+            while (numero != 0)
+            {
+                resto = numero % divisore;
+                numero = numero / 16;
+                numeroConvertito = Convert.ToString(resto) + numeroConvertito;
+                if (resto > 9)
+                {
+                    if (resto == 10)
+                    {
+                        numeroConvertito = "A" + numeroConvertito;
+                    }
+                    else if (resto == 11)
+                    {
+                        numeroConvertito = "B" + numeroConvertito;
+                    }
+                    else if (resto == 12)
+                    {
+                        numeroConvertito = "C" + numeroConvertito;
+                    }
+                    else if (resto == 13)
+                    {
+                        numeroConvertito = "D" + numeroConvertito;
+                    }
+                    else if (resto == 14)
+                    {
+                        numeroConvertito = "E" + numeroConvertito;
+                    }
+                    else if (resto == 15)
+                    {
+                        numeroConvertito = "F" + numeroConvertito;
+                    }
+                    else
+                    {
+                        numeroConvertito = Convert.ToString(resto) + numeroConvertito;
+                    }
+                }
+            }
+            Console.WriteLine($"Il numero convertito è {numeroConvertito}");
+            Console.ReadLine();
         }
     }
 }
